63b4214cfa8337ac2a56e33d0a26ffb2a56c5025
Leonard-Theisler <s204688@dtu.dk>
2022-11-28 14:54:33 +0100

    Basic user input acheived
    Added a text box in which the user can write, the button then saves the input and converts it to a std::string (from Qstring). Will be useful to open files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "./ui_mainwindow.h"
 #include <iostream>
 #include <QDebug>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -18,6 +19,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    qDebug() << "Hello";
+
+    /*prints hello on the console and opens a dialogue box (completely useless, just experimenting)
+     *
+     * qDebug() << "Hello";
+    QMessageBox::information(this, "Message", "Check the console", QMessageBox::Ok);*/
+
+    userText= ui->plainTextEdit->toPlainText();
+    qDebug() << "Text: " << userText;
+
+    filename = userText.toStdString();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTextEdit>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -18,7 +19,10 @@ public:
 private slots:
     void on_pushButton_clicked();
 
+
 private:
     Ui::MainWindow *ui;
+    QString userText;
+    std::string filename;
 };
 #endif // MAINWINDOW_H
