fd3c0dae4bffaec52a69369aff464b5dda65610e
DuesselbergAdrian <a.duesselberg@gmail.com>
2022-11-29 20:48:18 +0100

    Update Read_Visualize_CSV.cpp

--- Read_Visualize_CSV.cpp
+++ Read_Visualize_CSV.cpp
@@ -2,16 +2,16 @@
 #include <fstream>
 #include <string>
 
+
 using namespace std;
 
 //Sources:
 //1. https://www.youtube.com/watch?v=TSRdO0AH9Gc
 //2. https://www.geeksforgeeks.org/csv-file-management-using-c/
 
-
+//read a csv file called fake_coordinates (2D)
 void read_csv_file(){
     string myFilePath = "fake_coordinates.csv";
-
     ifstream coordinates;
 
     coordinates.open(myFilePath);
@@ -19,7 +19,7 @@ void read_csv_file(){
     if(coordinates.fail()){
         cerr << "Unable to open file" << myFilePath << endl;
     }
-    while(coordinates.peek() != EOF){
+    while(coordinates.peek() != EOF){ //loop over the coordinates and print them
         string coordinate;
         getline(coordinates,coordinate,',');
         cout <<coordinate<< endl;
@@ -27,6 +27,7 @@ void read_csv_file(){
 
     coordinates.close();
 }
-void visualize(){
-    cout <<'Hello'<< endl;
+// how do we want to visualize it ?
+void visualize_csv(){
+
 }
