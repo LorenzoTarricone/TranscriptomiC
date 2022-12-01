847ddc362a73398a4e649a6932e0715cf84cbca3
DuesselbergAdrian <a.duesselberg@gmail.com>
2022-12-01 08:29:21 +0100

    Tutorial

--- Visualize.cpp
+++ Visualize.cpp
@@ -8,16 +8,16 @@ using namespace std;
 //Sources:
 //1. https://www.youtube.com/watch?v=TSRdO0AH9Gc
 //2. https://www.geeksforgeeks.org/csv-file-management-using-c/
+//2. https://towardsdatascience.com/data-preprocessing-and-visualization-in-c-6d97ed236f3b
 
 //Goal: Read a csv file and visualize the data
 //Right know we can only read data from the file fake_coordinates. The data is not really visualized. Its just printed.
 void CSV :: read_csv_file(string myFilePath){
     ifstream coordinates;
-
-    coordinates.open(myFilePath); // Here we open the file
+    coordinates.open(myFilePath); // open a file to perform read operation
 
     if(coordinates.fail()){
-        cerr << "Unable to open file" << myFilePath << endl;
+        cerr << "Unable to open file" << myFilePath << endl; //checking whether the file is open
     }
     while(coordinates.peek() != EOF){ //loop over the coordinates and print them BUT WE WANT TO STORE THEM OR VISUALIZE THEM DIRECTLY
         string coordinate;
@@ -33,24 +33,22 @@ void CSV :: visualize_csv(){
 }
 //Goal: Read a txt file and visualize the data
 //Right know we can only read data from the file fake_coordinates.
-void TXT :: read_csv_file(string myFilePath){
+void TXT :: read_txt_file(string myFilePath){
     ifstream coordinates;
-
-    coordinates.open(myFilePath);
+    coordinates.open(myFilePath); //open a file to perform read operation
 
     if(coordinates.fail()){
-        cerr << "Unable to open file" << myFilePath << endl;
-    }
-    while(coordinates.peek() != EOF){
-        string coordinate;
-        getline(coordinates,coordinate,',');
-        cout <<coordinate<< endl;
+        cerr << "Unable to open file" << myFilePath << endl; //checking whether the file is open
     }
 
-    coordinates.close();
+    string coordinate;
+    while(getline(coordinates, coordinate)){ //read data from file object and put it into string.
+         cout << coordinate << endl; // prints the data
+    }
+    coordinates.close(); //close the file object.
 }
 //Goal: Visualize csv file data
-void TXT :: visualize_csv(){
+void TXT :: visualize_txt(){
 
 }
 
