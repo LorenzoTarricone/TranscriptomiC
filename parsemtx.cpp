abdfcedecea7acb1640771efed5700e179338c8f
npeuker <nina.peuker@polytechnique.edu>
2022-12-15 11:26:29 +0100

    npeuker - read from .mtx file to Eigen sparse matrix, yet to be tested with proper .mtx file

--- parsemtx.cpp
+++ parsemtx.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <algorithm>
 #include <Eigen/Sparse>
+#include <stdio.h>
+
 
 parsemtx::parsemtx()
 {
@@ -25,8 +27,14 @@ void parsemtx::readFile(std::string filename){
 //    matrix = new double[M*N];	     // Creates the array of M*N size
 //    std::fill(matrix, matrix + M*N, 0.); // From <algorithm>, zeros all entries.
 
-    matrix = Eigen::MatrixXd(M,N);
-    Eigen::SparseMatrix<double> sparseMatrix = Eigen::SparseMatrix<double>(M,N);
+//    Eigen::MatrixXd fullmatrix = Eigen::MatrixXd(M,N);
+//    Eigen::SparseMatrix<double> sparseMatrix = Eigen::SparseMatrix<double>(M,N);
+
+    // from https://eigen.tuxfamily.org/dox/group__TutorialSparse.html
+    typedef Eigen::Triplet<double> T;
+    std::vector<T> tripletList;
+    int  estimation_of_entries = (int) ((1-0.98) * M * N);
+    tripletList.reserve(estimation_of_entries);
 
     // Read the data
     for (int l = 0; l < L; l++)
@@ -34,9 +42,26 @@ void parsemtx::readFile(std::string filename){
         int m, n;
         double data;
         fin >> m >> n >> data;
-        matrix(m,n) = data;
+        tripletList.push_back(T(m,n,data));
+//        fullmatrix(m,n) = data;
 //        matrix[(m-1) + (n-1)*M] = data;
     }
 
     fin.close();
+    Eigen::SparseMatrix<double> sparseMatrix(M,N);
+    sparseMatrix.setFromTriplets(tripletList.begin(), tripletList.end());
+
+    this->sparse = sparseMatrix;
+//    this->matrix =
+}
+
+void parsemtx::print(){
+    for (int k=0; k<sparse.outerSize(); ++k)
+      for (Eigen::SparseMatrix<double>::InnerIterator it(sparse,k); it; ++it)
+      {
+        std::cout << it.value() << " "
+        << it.row() << " "   // row index
+        << it.col() << " "   // col index (here it is equal to k)
+        << it.index() << std::endl; // inner index, here it is equal to it.row()
+      }
 }
--- parsemtx.h
+++ parsemtx.h
@@ -3,15 +3,19 @@
 
 #include <string>
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <iostream>
 
 class parsemtx
 {
 public:
     parsemtx();
     void readFile(std::string filename);
+    void print();
 
 private:
     Eigen::MatrixXd matrix;
+    Eigen::SparseMatrix<double> sparse;
 };
 
 #endif // PARSEMTX_H
