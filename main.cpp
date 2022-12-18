3677bfa04fc913c6f7baea83cfebaaba5ca395c4
Nicolas <nicolas.lopez-corral@polytechnique.edu>
2022-12-18 20:26:53 +0100

    Implemented a function to search genes using the api
    On my computer I still get "qt.tlsbackend.ossl: Failed to load libssl/libcrypto."
    Working on it.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,57 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QUrl>
+#include <QUrlQuery>
+
+QJsonDocument searchGenes(const QMap<QString, QString>& params)
+{
+    // create a network manager
+    QNetworkAccessManager manager;
+
+    // set up the API URL
+    QUrl url("https://clinicaltables.nlm.nih.gov/api/genes/v4/search");
+
+    // create a query object and add the parameters
+    QUrlQuery query;
+    for (auto it = params.begin(); it != params.end(); ++it) {
+        query.addQueryItem(it.key(), it.value());
+    }
+
+    // add the query to the URL
+    url.setQuery(query);
+
+    // create a request and set the URL
+    QNetworkRequest request(url);
+
+    // send the request and get the response
+    QNetworkReply *response = manager.get(request);
+    auto data = response->readAll();
+
+    // parse the response as a JSON document
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+
+    return doc;
+}
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    //Example call
+    QMap<QString, QString> params;
+    params["term"] = "BRCA1";
+    params["exact_match"] = "true";
+    QJsonDocument doc = searchGenes(params);
+    QString strJson(doc.toJson(QJsonDocument::Indented));
+    // print the string to the console
+    qDebug() << strJson;
+
     MainWindow w;
     w.show();
     return a.exec();
