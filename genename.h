3c8169da9902cd2eb704747579e9ed75bda3ca8c
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2022-12-18 21:38:04 +0100

    Update genename.h

--- genename.h
+++ genename.h
@@ -3,17 +3,19 @@
 #include <stdio.h>
 #include <iostream>
 #include <string.h>
-
+#include <set>
 using namespace std;
 
 class Genename
 {
 private:
     string true_list;
+    set<string> res;
 public:
     Genename();
     int isSubstring(string s1, string s2);
-    string listgene(string l, string search);
+    set<string> listgene(string l, string search);
+    void printset(set<string> result);
 };
 
 #endif // GENENAME_H
