7748142ce236aed6c94d6bc58a5af784bb39c171
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2022-12-18 21:37:23 +0100

    Update genename.cpp

--- genename.cpp
+++ genename.cpp
@@ -1,5 +1,5 @@
-/*What to put in the main:
-#include "genename.h"
+/* main to test listgene
+ * #include "genename.h"
 
 #include <iostream>
 #include <string>
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include<set>
 using namespace std;
 
 using std::cout; using std::cin;
@@ -18,16 +19,14 @@ using std::stringstream;
 int main(){
     string l="[136,[\"HGNC:35077\",\"HGNC:35019\",\"HGNC:77\",\"HGNC:35013\",\"HGNC:35114\",\"HGNC:24098\",\"HGNC:16638\"],null,[[\"TRR-CCT8-1\",\"tRNA-Arg-CCT-8-1\",\"TRNAR32P\"],[\"TRR-CCT6-2\",\"tRNA-Arg-CCT-6-2\",\"TRNAR31P\"],[\"ABL2\",\"ARG\",\"ABLL\"],[\"TRR-CCT7-1\",\"tRNA-Arg-CCT-7-1\",\"TRNAR30P\"],[\"TRR-CCT9-1\",\"tRNA-Arg-CCT-9-1\",\"TRNAR34P\"],[\"SORBS2\",\"ARGBP2|KIAA0777\",\"\"],[\"SRRM1\",\"SRM160|POP101|MGC39488\",\"\"]]]";
     string search="\"ARG\"";
-
     Genename test;
-
-    test.listgene(l, search);
-
+    set<string> result;
+    result=test.listgene(l, search);
+    test.printset(result);
     return 0;
 }
+
 */
-
-
 
 #include "genename.h"
 
@@ -37,6 +36,7 @@ int main(){
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <set>
 using namespace std;
 
 using std::cout; using std::cin;
@@ -72,7 +72,7 @@ int Genename::isSubstring(string s1, string s2)
     return -1;
 }
 
-string Genename::listgene(string l, string search){
+set<string> Genename::listgene(string l, string search){
     int o=0; //nb of [
     int c=0; //nb of ]
     string len;
@@ -103,9 +103,8 @@ string Genename::listgene(string l, string search){
         }
     }
     cout<<'\n';
-    cout<<len<<'\n';
-    cout<<index<<'\n';
-    cout<<name<<'\n';
+    cout<<len<<'\n'<<'\n';
+    cout<<index<<'\n'<<'\n';
 
     o=0;
     c=0;
@@ -124,17 +123,41 @@ string Genename::listgene(string l, string search){
         else if (c==o) {
             cout<<list<<'\n';
             int res = isSubstring(search,list);
-                if (res !=-1){
-                    cout<<"Found"<<'\n';
-                    true_list=list;
-                }
-                list="";
+            if (res !=-1){
+                cout<<"Found"<<'\n';
+                true_list=list;
             }
-
+            list="";
         }
 
-  cout<<true_list<<'\n';
+    }
+
+    string value;
+    set<string> res;
+    string val;
+    for (int i=0; i<true_list.length();i++){
+        value=true_list[i];
+        if (value==","){
+            res.insert(val);
+            val.clear();
+        }
+        else if (value!="\0" and value!="\""){
+            val+=value;
+            value.clear();
+        }
+    }
+    res.insert(val);
+
+    return res;}
 
 
-    return true_list;}
+void Genename::printset(set<string> result){
+    cout<<'\n'<<"Set is: { ";
+    for(auto& str: result)
+      {
+        std::cout << str << ' ';
+      }
+    cout<<"}";
+}
+
 
