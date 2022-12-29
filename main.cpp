32fa8ee3fa7466940f743e5c6cfec7b8697f4339
Iris Brun <115143008+BRUNiris@users.noreply.github.com>
2022-12-29 13:11:45 +0100

    Add files via upload
    transforms gene matrix into linkage matrix

--- main.cpp
+++ main.cpp
@@ -1,21 +1,63 @@
-#include <QCoreApplication>
-#include "Visualize.hpp"
+#include "mainwindow.h"
+#include <QApplication>
+#include <Eigen/Dense>
+#include <cmath>
 
-
-
+using namespace Eigen;
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    MatrixXd A = openData("/Users/alanpicucci/Desktop/Projects/Transcriptomics/TranscriptomiC/dummyData.csv");
+    cout << A;
 
-    /*//Code to read and visualize the data
-    //start
-    string myFilePath = "fake_coordinates.csv"; // get file fake_coordinates.csv
-    CSV b;
-    b.read_csv_file(myFilePath); // read data
-    //end*/
+    cout << "\nRow 35, column 2 of matrix A: \n" <<A(34,1);
+
+    cout << "Transpose: " << A.transpose();
 
 
-    return a.exec();
+    //QApplication a(argc, argv);
+    //MainWindow w;
+    //w.show();
+    //return a.exec();
+}
+
+//returns the distance of coordinates x and y
+double distance(double x1, double y1, double x2, double y2) {
+    return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+
+//given an eigen matrix, returns an eigen matrix with the distance between points of the previous eigen matrix
+MatrixXd matrix_distance(MatrixXd A){
+    MatrixXd A_distance = A;
+    for (int i = 0; i < A.rows(); i++){
+        for (int j = 0; j < A.cols(); j++){
+            if (i != j) { //skip the distance for a point and itself
+                A_distance(i,j) = distance(A(i,0), A(i,1), A(j,0), A(j,1)); //get distance between two points
+            }
+        }
+    }
+    return A_distance;
+}
+
+//returns the max of two doubles
+double max (double a, double b){
+    if (a<b){return b;}
+    return a;
+}
+
+//returns the linkage value of coordinates x=(x1,y1) and y=(x2,y2) given m and p
+double linkage(double x1, double y1, double x2, double y2, double m, double p) {
+    return pow((max(0,m-distance(x1,x2,y1,y2))/m), p)*100;
+}
+
+//given an eigen matrix of the distance between points and the values m and p, returns an eigen matrix with the linkage values
+MatrixXd matrix_linkage(MatrixXd A, double m, double p){
+    MatrixXd A_linkage = A;
+    for (int i = 0; i < A.rows(); i++){
+        for (int j = 0; j < A.cols(); j++){
+            A_linkage(i,j) = linkage(A(i,0), A(i,1), A(j,0), A(j,1), m, p);
+        }
+    }
+    return A_linkage;
 }
