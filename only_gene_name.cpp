9fffb2bb63c6ce3c0a15db3d93b2e7d213810f9b
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2023-01-12 13:35:52 +0100

    Update only_gene_name.cpp

--- only_gene_name.cpp
+++ only_gene_name.cpp
@@ -11,12 +11,7 @@
 #include<set>
 using namespace std;
 
-using std::cout; using std::cin;
-using std::endl; using std::string;
-using std::vector; using std::istringstream;
-using std::stringstream;
-
-
+//For gene names
 int main(){
     string l="[136,[\"HGNC:35077\",\"HGNC:35019\",\"HGNC:77\",\"HGNC:35013\",\"HGNC:35114\",\"HGNC:24098\",\"HGNC:16638\"],null,[[\"TRR-CCT8-1\",\"tRNA-Arg-CCT-8-1\",\"TRNAR32P\"],[\"TRR-CCT6-2\",\"tRNA-Arg-CCT-6-2\",\"TRNAR31P\"],[\"ABL2\",\"ARG\",\"ABLL\"],[\"TRR-CCT7-1\",\"tRNA-Arg-CCT-7-1\",\"TRNAR30P\"],[\"TRR-CCT9-1\",\"tRNA-Arg-CCT-9-1\",\"TRNAR34P\"],[\"SORBS2\",\"ARGBP2|KIAA0777\",\"\"],[\"SRRM1\",\"SRM160|POP101|MGC39488\",\"\"]]]";
     string search="\"ARG\"";
@@ -27,6 +22,48 @@ int main(){
     return 0;
 }
 
+//For list biological processes
+int main(){
+    string json = R"({"results": {
+    "reference": {
+        "organism": "Homo sapiens",
+        "mapped_count": 20589,
+        "unmapped_count": 0
+    },
+    "input_list": {
+        "organism": "Homo sapiens",
+        "mapped_count": 3,
+        "mapped_id": [
+            "GPI",
+            "NDRG1",
+            "VEGFA"
+        ],
+        "unmapped_count": 0
+    },
+    "result": [
+        {
+            "number_in_list": 2,
+            "fold_enrichment": 175.97435897435898,
+            "fdr": 0.6994197998675634,
+            "expected": 0.011365292146291709,
+            "number_in_reference": 78,
+            "pValue": 4.46030100036709E-5,
+            "term": {
+                "id": "GO:0043154",
+                "label": "negative regulation of cysteine-type endopeptidase activity involved in apoptotic process"
+            },
+            "plus_minus": "+"
+        }
+    ]
+}})";
+    string search1="\"number_in_list\"";
+    string search2="\"id\"";
+    string search3="\"label\"";
+    only_gene_name test;
+    set<Info> result;
+    result=test.list_bio_process(json, search1,search2,search3);
+    return 0;
+}
 */
 
 #include "only_gene_name.h"
@@ -66,12 +103,12 @@ int only_gene_name::isSubstring(string s1, string s2)
     return -1;
 }
 
+
 set<string> only_gene_name::listgene(string l, string search){
     //if the search parameter is not only in upper case
     string upper_search = search;
     transform(upper_search.begin(), upper_search.end(), upper_search.begin(), ::toupper);
     cout<<"UP"<<upper_search<<'\n';
-
 
     int o=0; //nb of [
     int c=0; //nb of ]
@@ -175,3 +212,44 @@ void only_gene_name::printset(set<string> result){
       }
     cout<<"}";
 }
+
+set<Info> only_gene_name::list_bio_process(string json, string search1, string search2, string search3){
+    cout<<json<<'\n';
+    set<Info> infoSet;
+    stringstream ss(json);
+    string line;
+    int number_in_list;
+    string id;
+    string label;
+
+    while(getline(ss, line)) {
+        if (isSubstring(search1,line) != -1) {
+            // Extract the number_in_list value
+            std::cout<<line<<'\n';
+            number_in_list = stoi(line.substr(line.find(":") + 1));}
+        else if (isSubstring(search2,line) != -1) {
+            // Extract the number_in_list value
+            std::cout<<line<<'\n';
+            id = line.substr(line.find(":") + 3, line.find(",") - line.find(":") - 4);
+        }
+        else if (isSubstring(search3,line) != -1) {
+            std::cout<<line<<'\n';
+            // Extract the number_in_list value
+            label=line.substr(line.find(":") + 2, line.find("\"") - line.find(":")-4);
+            // Add the extracted information to the set
+            infoSet.insert({number_in_list, id, label});
+
+            // Print the Info set
+            for (auto info : infoSet) {
+                cout << "number_in_list: " << info.number_in_list << endl;
+                cout << "id: " << info.id << endl;
+                cout << "label: " << info.label << endl;
+            }
+
+            return infoSet; //we want to stop the code as soon as a label is find
+        }
+    }
+return infoSet;
+}
+
+
