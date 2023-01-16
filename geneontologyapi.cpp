3d6654a42ada20f03f7763263a2a182de36dac59
Iris Brun <115143008+BRUNiris@users.noreply.github.com>
2023-01-16 14:17:03 +0100

    Update geneontologyapi.cpp

--- geneontologyapi.cpp
+++ geneontologyapi.cpp
@@ -3,6 +3,12 @@
 #include <fstream>
 using namespace std;
 
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
 int main () {
     CURL* curl = curl_easy_init(); //Initialize curl
     std::string response;
