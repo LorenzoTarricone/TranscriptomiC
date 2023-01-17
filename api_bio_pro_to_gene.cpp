cfb6b02f7c4751d98c299ccc594b06beb3cd05b5
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2023-01-17 20:16:14 +0100

    create a new method for the API of the bio pro

---
+++ api_bio_pro_to_gene.cpp
@@ -0,0 +1,95 @@
+/*Take a file.txt (list of genes linked to a specific biopro)+ file.tsv (list of genes in matrix table)
+ * and returns the names that are in both (after calling an API to have all the different names of the genes)
+ **/
+#include "api_bio_pro_to_gene.h"
+#include "readgenetxt.h"
+#include "only_gene_name.h"
+#include "read_tsv_set.h"
+#include "intersection_sets.h"
+#include "api.cpp"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <QJsonDocument>
+#include <set>
+
+api_bio_pro_to_gene::api_bio_pro_to_gene()
+{
+
+}
+
+std::vector<std::string> api_bio_pro_to_gene::api_bio_pro_to_gene_function(std::string geneNameFile, std::string geneBioProFile){
+
+    //First we read the txt file with the gene name for a particular biologicl process
+    //And we take all the name of these genes
+
+    readgenetxt vec;
+    std::vector<std::string> res;
+    res=vec.listgene_bio_pro(geneNameFile,500); //list of all the genes in the txt file, here 500 bc I want to test with only 500 genes linked to the biological process
+    std::cout<<"[";
+    for (const std::string& i : res) {
+        std::cout << i<<" ";
+      }
+    std::cout<<"]";
+
+    std::set<std::string> final_set;
+    for (unsigned int i=0; i<res.size();i++){
+        std::string search=res[i];
+        qDebug() << QString::fromStdString(search);
+        std::string l;
+        //API call
+
+        QMap<QString, QString> params;
+        params["terms"]= QString::fromStdString(search);
+        qDebug() <<params;
+        QJsonDocument doc = searchHGNC(params);
+        QString strJson(doc.toJson(QJsonDocument::Compact));
+        l=strJson.toStdString();
+        std::cout<<"THIS IS" << search<<l;
+
+        only_gene_name test; //search other name of this specific gene in the string l
+        std::set<std::string> small_set;
+        small_set=test.listgene(l, search);
+        test.printset(small_set);
+
+        final_set.insert(small_set.begin(), small_set.end()); //add the set of the names of this gene in the set of all the genes
+
+        std::cout<<'\n'<<"Set is: { "; //print final set
+        for(auto& str: final_set)
+          {
+            std::cout << str << ' ';
+          }
+        std::cout<<"}";
+
+    }
+
+    //Now let us create the set of all the gene of the matrix file
+    read_tsv_set test2;
+    std::set<std::string> string_set_gene_matrix;
+    string_set_gene_matrix=test2.read_tsv(geneBioProFile);
+
+    //Now we take the intersection of the 2 sets, to keep only the genes we are interrested in
+    intersection_sets test3;
+    std::set<std::string> intersection_set;
+    intersection_set = test3.set_intersection(final_set, string_set_gene_matrix);
+
+    //now we remove the sets and vectors we don't use anymore to free memory
+    res.~vector();
+    small_set.~set();
+    final_set.~set();
+    string_set_gene_matrix.~set();
+
+    for (std::string x : intersection_set) {
+        std::cout << x << " ";
+    }
+
+    //return a vector instead of a set since the backend team was working with a set
+    std::vector<string> v(intersection_set.begin(), intersection_set.end());
+
+    return v;}
+
---
+++ api_bio_pro_to_gene.h
@@ -0,0 +1,13 @@
+#ifndef API_BIO_PRO_TO_GENE_H
+#define API_BIO_PRO_TO_GENE_H
+
+
+class api_bio_pro_to_gene
+{
+public:
+    api_bio_pro_to_gene();
+    std::vector<std::string> api_bio_pro_to_gene_function(std::string geneNameFile, std::string geneBioProFile){
+
+};
+
+#endif // API_BIO_PRO_TO_GENE_H
