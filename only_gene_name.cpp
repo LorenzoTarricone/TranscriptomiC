a87f901d7677dfdca4ca1ede6f584ce3d9845a67
clemence-mottez <115174132+clemence-mottez@users.noreply.github.com>
2023-01-18 16:58:38 +0100

    add comments

--- only_gene_name.cpp
+++ only_gene_name.cpp
@@ -1,33 +1,7 @@
 
-/* main to test listgene
- * #include "only_gene_name.h"
-
-#include <iostream>
-#include <string>
-#include <sstream>
-#include <iterator>
-#include <vector>
-#include <algorithm>
-#include<set>
-using namespace std;
-
-using std::cout; using std::cin;
-using std::endl; using std::string;
-using std::vector; using std::istringstream;
-using std::stringstream;
+//Parse the “API answer” to only keep some informations (for both gene name and biological process API)
 
 
-int main(){
-    string l="[136,[\"HGNC:35077\",\"HGNC:35019\",\"HGNC:77\",\"HGNC:35013\",\"HGNC:35114\",\"HGNC:24098\",\"HGNC:16638\"],null,[[\"TRR-CCT8-1\",\"tRNA-Arg-CCT-8-1\",\"TRNAR32P\"],[\"TRR-CCT6-2\",\"tRNA-Arg-CCT-6-2\",\"TRNAR31P\"],[\"ABL2\",\"ARG\",\"ABLL\"],[\"TRR-CCT7-1\",\"tRNA-Arg-CCT-7-1\",\"TRNAR30P\"],[\"TRR-CCT9-1\",\"tRNA-Arg-CCT-9-1\",\"TRNAR34P\"],[\"SORBS2\",\"ARGBP2|KIAA0777\",\"\"],[\"SRRM1\",\"SRM160|POP101|MGC39488\",\"\"]]]";
-    string search="\"ARG\"";
-    only_gene_name test;
-    set<string> result;
-    result=test.listgene(l, search);
-    test.printset(result);
-    return 0;
-}
-
-*/
 
 #include "only_gene_name.h"
 
@@ -45,6 +19,7 @@ only_gene_name::only_gene_name()
 
 }
 
+//check if a string is a substring of another
 int only_gene_name::isSubstring(string s1, string s2)
 {
     int M = s1.length();
@@ -64,6 +39,7 @@ int only_gene_name::isSubstring(string s1, string s2)
     return -1;
 }
 
+//parse the API answer for gene name
 std::set<std::string> only_gene_name::listgene(std::string l, std::string search){
     //if the search parameter is not only in upper case
     std::string upper_search = search;
@@ -164,7 +140,49 @@ std::set<std::string> only_gene_name::listgene(std::string l, std::string search
     }
     return res;}
 
+//parse the API answer for biological processe
+set<Info> only_gene_name::list_bio_process(string json, string search1, string search2, string search3){
+    cout<<json<<'\n';
+    set<Info> infoSet;
+    stringstream ss(json);
+    string line;
+    int number_in_list;
+    string id;
+    string label;
 
+    while(getline(ss, line)) {
+        if (isSubstring(search1,line) != -1) {
+            // Extract the number_in_list value
+            std::cout<<line<<'\n';
+            number_in_list = stoi(line.substr(line.find(":") + 1));}
+        else if (isSubstring(search2,line) != -1) {
+            // Extract the number_in_list value
+            std::cout<<line<<'\n';
+            id = line.substr(line.find(":") + 3, line.find(",") - line.find(":") - 4);
+        }
+        else if (isSubstring(search3,line) != -1) {
+            std::cout<<line<<'\n';
+            // Extract the number_in_list value
+            label=line.substr(line.find(":") + 2, line.find("\"") - line.find(":")-4);
+            // Add the extracted information to the set
+            infoSet.insert({number_in_list, id, label});
+
+            // Print the Info set
+            for (auto info : infoSet) {
+                cout << "number_in_list: " << info.number_in_list << endl;
+                cout << "id: " << info.id << endl;
+                cout << "label: " << info.label << endl;
+            }
+
+            return infoSet; //we want to stop the code as soon as a label is find
+        }
+    }
+return infoSet;
+}
+
+
+
+//print a set
 void only_gene_name::printset(std::set<std::string> result){
     std::cout<<'\n'<<"Set is: { ";
     for(auto& str: result)
@@ -173,3 +191,33 @@ void only_gene_name::printset(std::set<std::string> result){
       }
     std::cout<<"}";
 }
+
+
+/* main to test listgene
+ * #include "only_gene_name.h"
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <vector>
+#include <algorithm>
+#include<set>
+using namespace std;
+
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::vector; using std::istringstream;
+using std::stringstream;
+
+
+int main(){
+    string l="[136,[\"HGNC:35077\",\"HGNC:35019\",\"HGNC:77\",\"HGNC:35013\",\"HGNC:35114\",\"HGNC:24098\",\"HGNC:16638\"],null,[[\"TRR-CCT8-1\",\"tRNA-Arg-CCT-8-1\",\"TRNAR32P\"],[\"TRR-CCT6-2\",\"tRNA-Arg-CCT-6-2\",\"TRNAR31P\"],[\"ABL2\",\"ARG\",\"ABLL\"],[\"TRR-CCT7-1\",\"tRNA-Arg-CCT-7-1\",\"TRNAR30P\"],[\"TRR-CCT9-1\",\"tRNA-Arg-CCT-9-1\",\"TRNAR34P\"],[\"SORBS2\",\"ARGBP2|KIAA0777\",\"\"],[\"SRRM1\",\"SRM160|POP101|MGC39488\",\"\"]]]";
+    string search="\"ARG\"";
+    only_gene_name test;
+    set<string> result;
+    result=test.listgene(l, search);
+    test.printset(result);
+    return 0;
+}
+*/
