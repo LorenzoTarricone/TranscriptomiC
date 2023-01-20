6d19672359382d5b843061424d0856cde2f9ece7
Nicolas <nicolas.lopez-corral@polytechnique.edu>
2023-01-20 23:44:29 +0100

    Update api.cpp

--- api.cpp
+++ api.cpp
@@ -1,16 +1,30 @@
 #include <QJsonDocument>
+#include <QJsonObject>
 #include <curl/curl.h>
 #include <QByteArray>
 #include <QUrl>
 #include <QUrlQuery>
+#include <iostream>
+#include <chrono>
+#include <ostream>
+#include <fstream>
+#include <QCoreApplication>
+#include <QJsonArray>
+#include <list>
+using namespace std::chrono;
+using namespace std;
 
 // Function to be called by libcurl when there is data to be written
 size_t write_data(void *ptr, size_t size, size_t nmemb, QByteArray *data) {
     data->append((char*) ptr, size * nmemb);
+    // qDebug() << QString(QByteArray((char*) ptr, size * nmemb));
+    // std::cin.get();
     return size * nmemb;
 }
 
 QByteArray downloadUrl(const QUrl &url) {
+    qDebug() << "Downloading from" << url.toString().toStdString().c_str();
+    auto start = high_resolution_clock::now();
     CURL *curl = curl_easy_init();
     QByteArray data;
     if (curl) {
@@ -20,13 +34,16 @@ QByteArray downloadUrl(const QUrl &url) {
         curl_easy_perform(curl);
         curl_easy_cleanup(curl);
     }
+    auto stop = high_resolution_clock::now();
+    qDebug() << "Downloaded" << data.size() << "bytes in" << duration_cast<seconds>(stop - start).count() << "seconds";
     return data;
 }
 
 QJsonDocument searchHGNC(QMap<QString, QString> params) {
     QUrl url("https://clinicaltables.nlm.nih.gov/api/genes/v4/search");
     params["sf"] = "symbol,alias_symbol";
-    params["df"] = "symbol,alias_symbol";QUrlQuery query;
+    params["df"] = "symbol,alias_symbol";
+    QUrlQuery query;
     for (auto it = params.begin(); it != params.end(); ++it) {
         query.addQueryItem(it.key(), it.value());
     }
@@ -37,24 +54,45 @@ QJsonDocument searchHGNC(QMap<QString, QString> params) {
     return doc;
 }
 
-QJsonDocument searchDavid(QMap<QString, QString> params) {
-    QUrl url("https://clinicaltables.nlm.nih.gov/api/genes/v4/search");
+QJsonDocument searchPanther(QString geneInputList) {
+    QUrl url("http://pantherdb.org/services/oai/pantherdb/enrich/overrep");
+    QMap<QString, QString> params;
+    params["annotDataSet"] = "GO:0008150";
+    params["organism"] = "9606";
+    params["geneInputList"] = geneInputList;
     QUrlQuery query;
     for (auto it = params.begin(); it != params.end(); ++it) {
         query.addQueryItem(it.key(), it.value());
     }
     url.setQuery(query);
     QByteArray data = downloadUrl(url);
+    QString string = QString::fromUtf8(data.constData(), data.size());
+    // qDebug() << string;
     QJsonDocument doc = QJsonDocument::fromJson(data);
     return doc;
 }
 
+void saveTxt(QByteArray data, QString filename) {
+    std::ofstream outfile(filename.toStdString(), ios::out);
+    // qDebug() << "data is" << data.data();
+    outfile << data.data() << std::endl;
+    outfile.close();
+}
+
+void writeGeneontology() {
+    QUrl url("http://current.geneontology.org/ontology/go.obo");
+    QByteArray data = downloadUrl(url);
+}
+
+vector<string> getOverrep(vector<string> geneLists) {
+    vector<string> labelList = {};
+    for (vector<string>::iterator geneList = geneLists.begin(); geneList != geneLists.end(); ++geneList) {
+        QJsonDocument doc = searchPanther(QString::fromStdString(*geneList));
+        labelList.push_back(doc.object()["results"].toObject()["result"].toArray().first().toObject()["term"].toObject()["label"].toString().toStdString());
+    } return labelList;
+}
 
 void printExample() {
-    //Example call
-    QMap<QString, QString> params;
-    params["terms"] = "BRCA1";
-    QJsonDocument doc = searchHGNC(params);
-    qDebug() << "API Test";
-    qDebug() << qPrintable(doc.toJson());
 }
+
+void test() {}
